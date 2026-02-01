b390528879a0ed4166fa137878e4a28053053c48
umrzoq-toshkentov <ssdumrzoq@gmail.com>
2026-02-01 15:35:10 +0500

    Initialized project

--- src/index.ts
+++ src/index.ts
@@ -7,32 +7,18 @@ if (!BOT_TOKEN) {
 }
 
 const bot = new Telegraf(BOT_TOKEN);
+bot.start((ctx) => {
+  ctx.reply("Hello!");
+});
 
-// Log all incoming messages to terminal
-bot.on("message", (ctx) => {
-  const message = ctx.message;
-  const from = ctx.from;
+bot.help((ctx) => {
+  ctx.reply("Help message");
+});
 
-  console.log(`[${new Date().toISOString()}] Message from ${from?.first_name} (${from?.id}):`);
-
-  if ("text" in message) {
-    console.log(`  Text: ${message.text}`);
-  } else if ("photo" in message) {
-    console.log(`  Photo received`);
-  } else if ("sticker" in message) {
-    console.log(`  Sticker: ${message.sticker.emoji}`);
-  } else if ("document" in message) {
-    console.log(`  Document: ${message.document.file_name}`);
-  } else {
-    console.log(`  Other message type`);
-  }
-  console.log("");
+bot.command("hello", (ctx) => {
+  ctx.reply("Hello from bot!");
 });
 
 bot.launch();
-
-// Graceful shutdown
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
 console.log("Bot is running...");
